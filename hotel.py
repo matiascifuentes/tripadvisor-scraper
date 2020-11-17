2b96c7441acbd719ae2a02ebf3c8da672170ad77
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-11-17 12:22:17 -0300

    Creación de script hotel, defino los campos a extraer

---
+++ hotel.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = "Matias Cifuentes Lara"
+
+import scrapy   
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+import file_utils
+
+class TripadvisorItem(scrapy.Item):
+	nombre = scrapy.Field()
+	url = scrapy.Field()
+	descripcion = scrapy.Field()
+	ciudad = scrapy.Field()
+	direccion = scrapy.Field()
+	num_valoracion = scrapy.Field()
+	valoracion = scrapy.Field()
+
