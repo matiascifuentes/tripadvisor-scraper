1042a3e9ff1fd8e32b9b2c91d55d1efaac925862
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2020-11-17 12:19:33 -0300

    Agrego función insertar_datos en file_utils

--- file_utils.py
+++ file_utils.py
@@ -14,3 +14,17 @@ def leer_datos(nombre_archivo):
 	archivo.close()
 	return array
 
+def insertar_datos(nombre_archivo,cadena):
+	"""Inserta un string en una fila de un archivo txt.
+	Argumentos:
+	nombre_archivo: String. No debe tener la extension.
+	cadena: String a escribir en el archivo.
+	"""
+
+	archivo = open(nombre_archivo + '.txt', "r+")
+	contenido = archivo.read()
+	final_de_archivo = archivo.tell()
+	archivo.seek(final_de_archivo)
+	archivo.write(cadena)
+	archivo.write('\n')
+	archivo.close()
